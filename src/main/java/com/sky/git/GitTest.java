388943ecab717523a3fff8bce544670c04ffd96e
csky233 <112803459+csky233@users.noreply.github.com>
2022-09-05 16:07:38 +0800

    pull test

--- src/main/java/com/sky/git/GitTest.java
+++ src/main/java/com/sky/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("master idea push test");
+        System.out.println("master idea pull test");
     }
 }
